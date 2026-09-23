f11871729a59b0794e7519b393eee8b755e15e33
RozimuhammadDev09 <akramjonovrozimuhammad4@gmail.com>
2026-09-23 20:17:17 +0500

    yanbg

--- session.py
+++ session.py
@@ -1,8 +1,8 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
-api_id = 32924078
-api_hash = "5fb624acedb64de522eff541a4b6d7f5"
+api_id = 38017100
+api_hash = "0d1ee14a452e04c86c4dd37709bb7a2f"
 
 with TelegramClient(StringSession(), api_id, api_hash) as client:
     print(client.session.save())
